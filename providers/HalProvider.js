3170bd471740ebdb887d6813476bcdaf438b9d2e
zaro <zaro.megagenta@gmail.com>
2017-04-29 00:06:15 +0700

    fix(provider): Remove boot method

--- providers/HalProvider.js
+++ providers/HalProvider.js
@@ -12,15 +12,6 @@
 const ServiceProvider = require('adonis-fold').ServiceProvider;
 
 class HalProvider extends ServiceProvider {
-  * boot () {
-    const Response = this.app.use('Adonis/Src/Response');
-    const HalTransformer = this.app.use('Adonis/Resp/Hal');
-    yield HalTransformer.transform('a');
-    Response.macro('hal', function (json) {
-      this.json(json);
-    });
-  }
-
   * register () {
     this.app.singleton('Adonis/Resp/Hal', function (app) {
       const HalMiddleware = require('../src/HalMiddleware');
